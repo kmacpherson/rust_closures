78d118532afaf3b27434e88d7b28199321861662
Kevin MacPherson <kevin@teamroke.ca>
2019-08-12 22:13:37 -0400

    Changing numbers to validate closure.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let simulated_user_specified_value = 15;
+    let simulated_random_number = 2;
 
     generate_workout(
         simulated_user_specified_value,
